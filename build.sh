ca43dfcbab61d9eacc56d76873905374c42a1596
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-07-14 19:09:49 -0400

    Add docker images for various version combinations of caffe and PyGreentea

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t turagalab/greentea:cudnn5-caffe_gt ./cudnn5-caffe_gt
+docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt ./cudnn5-caffe_gt/pygt
+docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.6 ./cudnn5-caffe_gt/pygt-0.6
