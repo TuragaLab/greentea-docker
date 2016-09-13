6092820f6ef75968f4ee2df2707b1d53d57453c1
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-09-13 14:20:05 -0400

    Add PyGreentea 0.9[b]

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 docker build -t turagalab/greentea:cudnn5-caffe_gt ./cudnn5-caffe_gt
 docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt ./cudnn5-caffe_gt/pygt
 docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.6 ./cudnn5-caffe_gt/pygt-0.6
+docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9 ./cudnn5-caffe_gt/pygt-0.9
+docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9b ./cudnn5-caffe_gt/pygt-0.9b
+
