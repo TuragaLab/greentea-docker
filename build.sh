c628f78d1cf9d4cf207f9951318637f98db6f4d8
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-10-20 02:09:24 -0400

    Adding turagalab/greentea:cudnn5-caffe_gt-pygt-0.9d

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
-docker build -t turagalab/greentea:cudnn5-caffe_gt ./cudnn5-caffe_gt
-docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt ./cudnn5-caffe_gt/pygt
-docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.6 ./cudnn5-caffe_gt/pygt-0.6
-docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9 ./cudnn5-caffe_gt/pygt-0.9
-docker build -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9b ./cudnn5-caffe_gt/pygt-0.9b
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-16.10.05              ./cudnn5-caffe_gt-16.10.05
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-16.10.05-pygt-0.9.1   ./cudnn5-caffe_gt-16.10.05/pygt-0.9.1
 
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt                       ./cudnn5-caffe_gt
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-pygt                  ./cudnn5-caffe_gt/pygt
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.6              ./cudnn5-caffe_gt/pygt-0.6
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9              ./cudnn5-caffe_gt/pygt-0.9
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9b             ./cudnn5-caffe_gt/pygt-0.9b
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9c             ./cudnn5-caffe_gt/pygt-0.9c
+docker build                   -t turagalab/greentea:cudnn5-caffe_gt-pygt-0.9d             ./cudnn5-caffe_gt/pygt-0.9d
