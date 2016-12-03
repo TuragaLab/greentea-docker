08ef398fc4fc23121392095d68c385d6a4a4ea53
William Grisaitis <grisaitisw@janelia.hhmi.org>
2016-12-02 22:47:09 -0500

    Add image for caffe from 12/2 - turagalab/greentea:libdnn-caffe_gt-2016.12.02

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-2016.12.02                 ./cudnn5-caffe_gt-2016.12.02
+docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-2016.12.02-pygt-0.9.4b     ./cudnn5-caffe_gt-2016.12.02/pygt-0.9.4b
+
 docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-2016.10.27                 ./cudnn5-caffe_gt-2016.10.27
 docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-2016.10.27-pygt-0.9.2b     ./cudnn5-caffe_gt-2016.10.27/pygt-0.9.2b
 docker build --pull --no-cache -t turagalab/greentea:cudnn5-caffe_gt-2016.10.27-pygt-0.9.3b     ./cudnn5-caffe_gt-2016.10.27/pygt-0.9.3b
